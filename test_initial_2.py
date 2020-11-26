90179ad0e585a58d3a814ec6e08858927f1baeec
marcosllessa <marcos1408lessa@gmail.com>
2020-11-26 16:10:13 -0300

    teste de conexao com o BD

--- test_initial_2.py
+++ test_initial_2.py
@@ -1,6 +1,18 @@
 
-def soma_1(numero):
-    return numero +1
 
-def test_soma_1():
-    assert soma_1(2) == 2
+
+def conexao():
+    import connection
+
+    connection = connection.conexao()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * from cnpj order by id_cnpj desc")
+    id_result = cursor.fetchone()
+    id_cnpj_in = id_result[0]
+    id_cnpj = id_cnpj_in + 1
+    return id_cnpj
+
+def test_de_conexao():
+
+    assert conexao != None
+
