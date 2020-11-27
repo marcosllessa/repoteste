025e6a51794f40de5c60177136b037dd451e1d60
marcosllessa <marcos1408lessa@gmail.com>
2020-11-27 18:24:51 -0300

    Add .gitignore __pycache__

---
+++ test_initial_2.py
@@ -0,0 +1,16 @@
+def conexao():
+    '''Teste de conexão com o BD'''
+    import connection
+
+    connection = connection.conexao()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * from cnpj order by id_cnpj desc")
+    id_result = cursor.fetchone()
+    id_cnpj_in = id_result[0]
+    id_cnpj = id_cnpj_in + 1
+    return id_cnpj
+
+def test_de_conexao():
+
+    assert conexao != None
+
